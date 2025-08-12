8201deed997fed0f36664ba533e440255bf7db7c
Gustavo Akira Uekita <akirauekita2002@gmail.com>
2025-08-12 18:32:16 -0300

    first commit

---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+import { RouterProvider } from "react-router-dom";
+import { router } from "../shared/routes";
+
+
+
+
+
+export default function App() {
+  return <RouterProvider router={router}/>;
+}
