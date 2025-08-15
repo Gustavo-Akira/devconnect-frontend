fa9583d7b9753955b008c0d51cfd2f986fae9bd2
Gustavo Akira Uekita <akirauekita2002@gmail.com>
2025-08-15 20:23:13 -0300

    chore: adding ts-node to the test action

--- src/features/home/__tests__/index.spec.tsx
+++ src/features/home/__tests__/index.spec.tsx
@@ -1,9 +1,9 @@
-import { render, screen } from "@testing-library/react";
-import { HomePage } from "../index";
-import "@testing-library/jest-dom";
+import { render, screen } from '@testing-library/react';
+import { HomePage } from '../index';
+import '@testing-library/jest-dom';
 
-describe("HomePage", () => {
-  it("render title", () => {
+describe('HomePage', () => {
+  it('render title', () => {
     render(<HomePage />);
     expect(screen.getByText(/Bem-vindo/i)).toBeInTheDocument();
     expect(screen.getByText(/DevConnect/i)).toBeInTheDocument();
@@ -11,28 +11,34 @@ describe("HomePage", () => {
 
   it("render button 'Comece agora'", () => {
     render(<HomePage />);
-    expect(screen.getByRole("button", { name: /Comece agora/i })).toBeInTheDocument();
+    expect(
+      screen.getByRole('button', { name: /Comece agora/i }),
+    ).toBeInTheDocument();
   });
 
-  it("render feature cards", () => {
+  it('render feature cards', () => {
     render(<HomePage />);
     expect(screen.getByText(/Encontre Devs/i)).toBeInTheDocument();
-    expect(screen.getByText("Publique Projetos",{exact:true})).toBeInTheDocument();
+    expect(
+      screen.getByText('Publique Projetos', { exact: true }),
+    ).toBeInTheDocument();
     expect(screen.getByText(/Conexões/i)).toBeInTheDocument();
     expect(screen.getByText(/Perfil Profissional/i)).toBeInTheDocument();
   });
 
-  it("render step cards", () => {
+  it('render step cards', () => {
     render(<HomePage />);
     expect(screen.getByText(/Como funciona/i)).toBeInTheDocument();
     expect(screen.getByText(/Crie seu perfil/i)).toBeInTheDocument();
-    expect(screen.getByText(/Encontre ou publique projetos/i)).toBeInTheDocument();
+    expect(
+      screen.getByText(/Encontre ou publique projetos/i),
+    ).toBeInTheDocument();
     expect(screen.getByText(/Colabore e cresça/i)).toBeInTheDocument();
   });
 
-  it("render images", () => {
+  it('render images', () => {
     render(<HomePage />);
-    const images = screen.getAllByRole("img");
+    const images = screen.getAllByRole('img');
     expect(images.length).toBeGreaterThan(0);
   });
 });
