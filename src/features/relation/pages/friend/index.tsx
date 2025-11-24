fc2763fdf064e761bab46e319d5d86e5f9d0b67f
Gustavo Akira Uekita <akirauekita2002@gmail.com>
2025-11-24 20:48:45 -0300

    refactor: separate logic from ui in friends page with userFriendsPage hook

--- src/features/relation/pages/friend/index.tsx
+++ src/features/relation/pages/friend/index.tsx
@@ -1,26 +1,10 @@
-import { useEffect, useState } from 'react';
-import { useAuth } from '../../../../shared/context/auth/authContext';
-import { getRecommendationsByProfile } from '../../../../shared/infra/services/relation/recommendationService';
-import type { Recommendations } from '../../../../shared/infra/services/relation/interface';
+import { useFriendsPage } from './hooks/useFriendsPage';
 
 export const FriendsPage = () => {
-  const { user } = useAuth();
-  const [recommendations, setRecommendations] = useState<Recommendations[]>([]);
-  useEffect(() => {
-    const userId = Number.parseInt(user!.id);
-    getRecommendationsByProfile(userId)
-      .then((data) => {
-        setRecommendations(data);
-        console.log('data ' + JSON.stringify(data));
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, [user]);
-
+  const { state } = useFriendsPage();
   return (
     <ul>
-      {recommendations.map((recommendation) => (
+      {state.recommendations.map((recommendation) => (
         <li key={recommendation.ID}>
           {recommendation.Name} - {recommendation.Score}
         </li>
